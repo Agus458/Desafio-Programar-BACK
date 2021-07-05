3f200c993e9286dcd590599145ed100755e8ac52
Agus458 <80288121+Agus458@users.noreply.github.com>
2021-07-05 23:15:30 +0100

    correccion post person

--- src/controllers/person.controller.ts
+++ src/controllers/person.controller.ts
@@ -5,55 +5,127 @@ import { Person } from "../models/Person";
 
 /* ----- Person Controller ----- */
 
-export const getPersons = async (request: Request, response: Response): Promise<Response> => {
-    return response.status(200).json(await getRepository(Person).find());
-}
+export const getPersons = async (
+  request: Request,
+  response: Response
+): Promise<Response> => {
+  return response.status(200).json(await getRepository(Person).find());
+};
 
-export const getPerson = async (request: Request, response: Response): Promise<Response> => {
-    return response.status(200).json(await getRepository(Person).findOne(request.params.id));
-}
+export const getPerson = async (
+  request: Request,
+  response: Response
+): Promise<Response> => {
+  return response
+    .status(200)
+    .json(await getRepository(Person).findOne(request.params.id));
+};
 
-export const createPerson = async (request: Request, response: Response): Promise<Response> => {
-    if (!request.body.name) return response.status(400).json({ message: 'No se ingresó el nombre de la persona' });
-    if (!request.body.lastname) return response.status(400).json({ message: 'No se ingresó el apellido de la persona' });
-    if (!request.body.email) return response.status(400).json({ message: 'No se ingresó el email de la persona' });
-    if (!request.body.phone) return response.status(400).json({ message: 'No se ingresó el telefono de la persona' });
+export const createPerson = async (
+  request: Request,
+  response: Response
+): Promise<Response> => {
+  if (!request.body.name)
+    return response
+      .status(400)
+      .json({ message: "No se ingresó el nombre de la persona" });
+  if (!request.body.lastname)
+    return response
+      .status(400)
+      .json({ message: "No se ingresó el apellido de la persona" });
+  if (!request.body.email)
+    return response
+      .status(400)
+      .json({ message: "No se ingresó el email de la persona" });
+  if (!request.body.phone)
+    return response
+      .status(400)
+      .json({ message: "No se ingresó el telefono de la persona" });
 
-    if (await getRepository(Person).findOne({ where: { email: request.body.email } })) return response.status(400).json({ message: 'Ya existe una persona con ese email' });
-    if (await getRepository(Person).findOne({ where: { phone: request.body.phone } })) return response.status(400).json({ message: 'Ya existe una persona con ese telefono' });
+  if (
+    await getRepository(Person).findOne({
+      where: { email: request.body.email },
+    })
+  )
+    return response
+      .status(400)
+      .json({ message: "Ya existe una persona con ese email" });
+  if (
+    await getRepository(Person).findOne({
+      where: { phone: request.body.phone },
+    })
+  )
+    return response
+      .status(400)
+      .json({ message: "Ya existe una persona con ese telefono" });
 
-    let newPerson = getRepository(Person).create(request.body);
-    let savedPerson = await getRepository(Person).save(newPerson);
+  let { name, lastname, email, phone } = request.body;
 
-    return response.status(201).json(savedPerson);
-}
+  let newPerson = getRepository(Person).create({
+    name,
+    lastname,
+    email,
+    phone,
+  });
+  let savedPerson = await getRepository(Person).save(newPerson);
 
-export const updatePerson = async (request: Request, response: Response): Promise<Response> => {
-    let person = await getRepository(Person).findOne(request.params.id);
+  return response.status(201).json(savedPerson);
+};
 
-    if (!person) return response.status(400).json({ message: 'No existe ninguna persona con ese id' });
+export const updatePerson = async (
+  request: Request,
+  response: Response
+): Promise<Response> => {
+  let person = await getRepository(Person).findOne(request.params.id);
 
-    if (request.body.name) person.name = request.body.name;
-    if (request.body.lastname) person.lastname = request.body.lastname;
-    if (request.body.email && request.body.email != person.email) {
-        if (await getRepository(Person).findOne({ where: { email: request.body.email } })) return response.status(400).json({ message: 'Ya existe una persona con ese email' });
-        person.email = request.body.email
-    };
-    if (request.body.phone && request.body.phone != person.phone) {
-        if (await getRepository(Person).findOne({ where: { phone: request.body.phone } })) return response.status(400).json({ message: 'Ya existe una persona con ese telefono' });
-        person.phone = request.body.phone
-    };
+  if (!person)
+    return response
+      .status(400)
+      .json({ message: "No existe ninguna persona con ese id" });
 
-    let updatedPerson = await getRepository(Person).save(person);
+  if (request.body.name) person.name = request.body.name;
+  if (request.body.lastname) person.lastname = request.body.lastname;
+  if (request.body.email && request.body.email != person.email) {
+    if (
+      await getRepository(Person).findOne({
+        where: { email: request.body.email },
+      })
+    )
+      return response
+        .status(400)
+        .json({ message: "Ya existe una persona con ese email" });
+    person.email = request.body.email;
+  }
+  if (request.body.phone && request.body.phone != person.phone) {
+    if (
+      await getRepository(Person).findOne({
+        where: { phone: request.body.phone },
+      })
+    )
+      return response
+        .status(400)
+        .json({ message: "Ya existe una persona con ese telefono" });
+    person.phone = request.body.phone;
+  }
 
-    return response.status(200).json(updatedPerson);
-}
+  let updatedPerson = await getRepository(Person).save(person);
 
-export const deletePerson = async (request: Request, response: Response): Promise<Response> => {
-    let person = await getRepository(Person).findOne(request.params.id);
-    if (!person) return response.status(400).json({ message: 'No existe ninguna persona con ese id' });
+  return response.status(200).json(updatedPerson);
+};
 
-    await getRepository(Business_Person).delete({person});
+export const deletePerson = async (
+  request: Request,
+  response: Response
+): Promise<Response> => {
+  let person = await getRepository(Person).findOne(request.params.id);
+  if (!person)
+    return response
+      .status(400)
+      .json({ message: "No existe ninguna persona con ese id" });
 
-    return response.status(200).json(await getRepository(Person).delete(request.params.id));
-}
+  await getRepository(Business_Person).delete({ person });
+
+  return response
+    .status(200)
+    .json(await getRepository(Person).delete(request.params.id));
+};
